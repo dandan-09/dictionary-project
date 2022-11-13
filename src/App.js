9543c850b58f1bddc6b21745d359e67e2e43831e
dandan-09 <103527768+dandan-09@users.noreply.github.com>
2022-11-13 20:49:54 +0000

    Created the search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <a
+            href="https://github.com/dandan-09/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source
+          </a>{" "}
+          code by Daniella Gombor
+        </footer>
+      </div>
     </div>
   );
 }
